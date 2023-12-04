daaf19e02e4bf959f5e76d473cc89a8883f5dcb3
Srini Pandiyan <srinipandiyan7@gmail.com>
2023-12-03 20:37:17 -0600

    last_login for user implemented in models

--- models/user.js
+++ models/user.js
@@ -44,7 +44,19 @@ class User {
 
   /** Update last_login_at for user */
 
-  static async updateLoginTimestamp(username) { }
+  static async updateLoginTimestamp(username) {
+    const result = await db.query(
+      `UPDATE users
+      SET last_login_at = current_timestamp
+      WHERE username = $1
+      RETURNING username`,
+      [username]
+    );
+    
+    if (!result.rows[0]) {
+      throw new ExpressError(`username does not exist: ${username}`, 404);
+    }
+  }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name, phone}, ...] */
