5db48f4158c0e1f3a007a31ed3c7ab7df90a2e39
Srini Pandiyan <srinipandiyan7@gmail.com>
2023-12-03 22:09:49 -0600

    added routes for messages.js

--- routes/messages.js
+++ routes/messages.js
@@ -1,3 +1,10 @@
+const Router = require("express").Router;
+const router = new Router();
+
+const Message = require("../models/message");
+const {ensureLoggedIn} = require("../middleware/auth");
+const ExpressError = require("../expressError");
+
 /** GET /:id - get detail of message.
  *
  * => {message: {id,
@@ -11,6 +18,18 @@
  *
  **/
 
+router.get("/:id", ensureLoggedIn, async function (req, res, next) {
+    try {
+        let username = req.user.username;
+        let msg = await Message.get(req.params.id);
+            if (msg.to_user.username !== username && msg.from_user.username !== username) {
+                throw new ExpressError("Unauthorized to read message", 401);
+            }
+        return res.json({message: msg});
+    } catch(e) {
+      return next(e);
+    }
+});
 
 /** POST / - post message.
  *
@@ -19,6 +38,18 @@
  *
  **/
 
+router.post("/", ensureLoggedIn, async function (req, res, next) {
+    try {
+        let msg = await Message.create({
+            from_username: req.user.username,
+            to_username: req.body.to_username,
+            body: req.body.body
+        });
+      return res.json({message: msg});
+    } catch(e) {
+      return next(e);
+    }
+});
 
 /** POST/:id/read - mark message as read:
  *
@@ -28,3 +59,21 @@
  *
  **/
 
+router.post("/:id/read", ensureLoggedIn, async function (req, res, next) {
+    try {
+        let username = req.user.username;
+        let msg = await Message.get(req.params.id);
+    
+            if (msg.to_user.username !== username) {
+                throw new ExpressError("Unable to mark message to read", 401);
+            }
+        
+        let message = await Message.markRead(req.params.id);
+        
+        return res.json({message});
+    } catch(e) {
+      return next(e);
+    }
+});
+
+module.exports = router;
